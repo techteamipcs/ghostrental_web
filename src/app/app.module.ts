6917881b7dc5abaa662d781130247ab75fe75f38
Akash Kurdekar <akashkurdekar39@gmail.com>
2025-05-10 13:08:32 +0530

    added lucide angular package for icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,9 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HttpClient, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { Error404Component } from './error404/error404.component';
 import { FormsModule } from '@angular/forms';
+import { BrandsComponent } from './components/brands/brands.component';
+import { LucideAngularModule, Fuel, RockingChair, Gauge, Star, ChevronRight, ChevronLeft } from 'lucide-angular';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
@@ -16,18 +19,22 @@ import { FormsModule } from '@angular/forms';
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    Error404Component
+    Error404Component,
+    BrandsComponent,
+    TestimonialsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    LucideAngularModule.pick({ Fuel, RockingChair, Gauge, Star, ChevronRight, ChevronLeft }),
   ],
   providers: [
     provideClientHydration(),
     provideHttpClient(withFetch())
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
